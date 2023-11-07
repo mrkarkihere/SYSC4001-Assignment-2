526b9488296888972ce8b9cfd40710891979c613
Arun Karki <arunkarki3629@gmail.com>
2023-11-06 23:00:22 -0500

    goodnight ml

--- server.c
+++ server.c
@@ -12,9 +12,22 @@ int message_get(){
 }
 
 // msg_snd() -> -1 or 0
-void message_send(){
+void message_send(struct msg_data* data){
     int msgqid = message_get();
-    int res = msgsnd(msgqid, );
+    if(msgsnd(msgqid, (void*) data, sizeof(*data), 0) == -1){
+        perror("msgsnd failed\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+// msg_rcv() -> # bytes received or -1
+void message_receive(struct msg_data *data, int* msg_key){
+    int msgqid = message_get();
+    // make changes to msg_key to properply function request-response model
+    if(msgrcv(msgqid, (void*) data, sizeof(*data), *msg_key, 0) == -1){
+        perror("msgrcv failed\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
 int main(){
